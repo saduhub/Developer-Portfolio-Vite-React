ecf07f8e3fed1abd472cfd5130a2e51ad058db0d
Jorge Duarte <sadujorge@gmail.com>
2024-03-05 15:59:08 -0800

    Imported assets, setup build, favicon, icons. Setup top level component and finished navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,55 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect } from 'react'
+import './assets/css/style.css'
+import favicon from './assets/pictures/svg/react.svg'
+
+function loadFavicon() {
+  const link = document.querySelector("link[rel~='icon']");
+  if (link) {
+    link.href = favicon;
+  }
+}
+
+function loadBoxicons() {
+  const link = document.createElement('link');
+  link.href = 'https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css';
+  link.rel = 'stylesheet';
+  document.head.appendChild(link);
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  // Add Link to head after component mounts with useEffect hook (side effect).  
+  useEffect(() => {
+    loadFavicon();
+    loadBoxicons();
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {/* Header */}
+      <header className="header-section">
+        <a href="/" className="logo">JD</a>
+        <input type="checkbox" id="checkbox"/>
+        <label htmlFor="checkbox" className="icons">
+          <i className='bx bx-menu' id="menu-icon"></i>
+          <i className='bx bx-x' id="close-icon"></i>
+        </label>
+        {/* Nav Bar */}
+        <nav className="navbar">
+          <div className="container-menu-items">
+            <a href="/" style={{ '--item': '0' }}>Home</a>
+            <a href="/aboutme" style={{ '--item': '0' }}>About Me</a>
+            <a href="/work" style={{ '--item': '1' }}>Work</a>
+            <a href="/contact" style={{ '--item': '2' }}>Contact</a>
+            <a href="/resume" style={{ '--item': '3' }}>Resume</a>
+          </div>
+        </nav>
+      </header>
+      {/* Main */}
+      <main className="main-container">
+        {/* Components here */}
+      </main>
+      {/* Footer */}
+      {/* Component Here */}
     </>
   )
 }
