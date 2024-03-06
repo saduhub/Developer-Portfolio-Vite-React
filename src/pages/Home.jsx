924fd06c902d39874dff741fd88deaa86df980e0
Jorge Duarte <sadujorge@gmail.com>
2024-03-06 12:06:40 -0800

    added home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,13 @@
+import '../assets/css/home.css';
+
 function Home() {
     return (
-      <div>
-        <h1>Home</h1>
-      </div>
+      <section className="home-section">
+          <div className="greeting">
+              <h2>Hello! My Name is Jorge Duarte and I am a Full-Stack Developer. </h2>
+          </div>
+          <a href="/work">See My Work!</a>
+      </section>
     );
 }
   
