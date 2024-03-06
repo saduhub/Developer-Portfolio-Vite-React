94d0705342d84485917119696211bf09963180f5
Jorge Duarte <sadujorge@gmail.com>
2024-03-06 13:13:21 -0800

    Finished resume page

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,8 +1,12 @@
+import '../assets/css/resume.css';
+import resume from  '../assets/resume.pdf'
+
 function Resume() {
     return (
-      <div>
-        <h1>Resume</h1>
-      </div>
+      <section className="resume-section">
+        <iframe className="resume-pdf" src={resume} title="Resume" style={{ border: 'none' }}></iframe>
+        <a href={resume} download>Download PDF</a>
+      </section>
     );
 }
   
