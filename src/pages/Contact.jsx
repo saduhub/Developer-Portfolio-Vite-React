e061133f86d7498849cd4991484c0d30e659cc1b
Jorge Duarte <sadujorge@gmail.com>
2024-03-06 13:51:23 -0800

    Finished contact page structure and syle. Need to setup backend to handle user input

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,22 @@
+import '../assets/css/contact.css'
+
 function Contact() {
-    return (
-      <div>
-        <h1>Contact</h1>
-      </div>
-    );
+  return (
+    <section className="contact-section"> 
+        <h3 className="work-header">Get in touch!</h3>
+        <form action="submit" className="contact-form">
+            <label htmlFor="name">Name:</label>
+            <input type="text" name="name" id="name" />
+            <label htmlFor="email">Email:</label>
+            <input type="email" name="email" id="email" />
+            <label htmlFor="subject">Subject:</label>
+            <input type="text" name="subject" id="subject" />
+            <label htmlFor="message">Message:</label>
+            <textarea name="message" id="message" /> 
+            <button type="submit">Send Message</button>
+        </form>
+    </section>
+  );
 }
-  
+
 export default Contact;
