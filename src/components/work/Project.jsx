dabb6052c11adca3b20e291c58e65e71800015fc
Jorge Duarte <sadujorge@gmail.com>
2024-03-06 22:00:21 -0800

    Added overlay with live site and repo links

--- src/components/work/Project.jsx
+++ src/components/work/Project.jsx
@@ -1,17 +1,26 @@
 const Project = ({ project }) => {
-    return (
-      <div className="project-div">
+  return (
+    <div className="project-div">
+      <div className="image-container">
+        {/* No hover on mobile, leave image inside anchor tag.*/}
         <a href={project.url} target="_blank" rel="noreferrer">
-          <img src={project.image} alt={`Screenshot of ${project.name} Application`} className="project-img" />
+          <img src={project.image} alt={`Screenshot of ${project.name}`} className="project-img" />
         </a>
-        <div className="tech-used">
-          {project.technologies.map((tech, index) => (
-            <img key={index} src={tech} alt="Technology Logo" />
-          ))}
+        <div className="overlay">
+          {/* GitHub link */}
+          <a href={project.url} target="_blank" rel="noreferrer">GitHub</a>
+          {/* Deployed link */}
+          <a href={project.deployed} target="_blank" rel="noreferrer">Live Site</a>
         </div>
-        <h4><span className="project-span">{project.name}</span> {project.description}</h4>
       </div>
-    );
+      <div className="tech-used">
+        {project.technologies.map((tech, index) => (
+          <img key={index} src={tech.src} alt={tech.alt} />
+        ))}
+      </div>
+      <h4><span className="project-span">{project.name}</span> {project.description}</h4>
+    </div>
+  );
 };
-  
+
 export default Project;
